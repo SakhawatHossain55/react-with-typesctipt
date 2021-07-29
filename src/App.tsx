729dae86f2f994e80d724019f2eb3537be6d99f6
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-07-29 21:32:42 +0600

    react with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddToList from './components/AddToList';
+import List from './components/List';
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
 
-
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      url: "https://cdn.pixabay.com/photo/2018/01/05/16/24/rose-3063284_960_720.jpg",
+      age: 36,
+      note: "Allegeric to staying on the same time"
+    }
+  ])
 
   return (
     <div className="App">
-      <h1>Pepole invited to me party</h1>
+      <h1>People Invited to me Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
